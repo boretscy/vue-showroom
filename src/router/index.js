7f2704dd27f79450fdbfab31588d8c804c8dd457
boretscy <boretscy@gmail.com>
2022-07-18 12:19:08 +0300

    m

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Item from '../views/Item.vue'
 import Items from '../views/Items.vue'
 import Favorites from '../views/Favorites.vue'
 import Compare from '../views/Compare.vue'
+import Test from '../views/Test.vue'
 
 Vue.use(VueRouter)
 
@@ -19,6 +20,11 @@ const routes = [
         path: '/compare',
         name: 'Compare',
         component: Compare
+    },
+    {
+        path: '/test',
+        name: 'Test',
+        component: Test
     },
     {
         path: '/favorites',
